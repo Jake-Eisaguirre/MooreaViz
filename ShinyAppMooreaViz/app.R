eefd8d724b8ea15947cdd42e9a19007d245f8097
charleshendrickson <c_d_h@bren.ucsb.edu>
2022-03-14 14:50:53 -0700

    created app.R

---
+++ ShinyAppMooreaViz/app.R
@@ -0,0 +1,55 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+# Load Packages ----
+library(shiny)
+library()
+
+
+
+# Load Data ----
+
+
+
+
+# User Interface ----
+# Creates the structure for your app's look and appearance 
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Moorea Coral Reef LTER"),
+
+     
+    
+
+    
+       
+    
+)
+
+
+
+# Server ----
+# Runs the r code to make the visualizations and transform the data for your app to function
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
