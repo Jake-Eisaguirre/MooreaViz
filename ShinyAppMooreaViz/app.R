43bc39711d9ae41b4ba1cc70ff12af2252db672d
jake-eisaguirre <eisaguirre@bren.ucsb.edu>
2022-04-05 12:57:09 -0700

    one layer added to map

--- ShinyAppMooreaViz/app.R
+++ ShinyAppMooreaViz/app.R
@@ -44,11 +44,57 @@ bleach <- read_csv(here("data", "csv", "percent_bleach_2016.csv")) %>%
 grd_sp <- readRDS(here("data", "krig_grid", "grd_sp"))
 
 #sewage data
-sewage_data <- read.csv(here("Data/Predicted_nuts.csv")) %>% 
+sewage_data <- read.csv(here("data/csv/Predicted_nuts.csv")) %>% 
     clean_names()
 
 sewage_data <- cbind(nitrogen_data, sewage_data)
 
+#raster brick minus lidar
+spatial_brick <- here("data", "spatial_brick.nc")
+
+spatial_brick <- brick(spatial_brick)
+
+# Tidy Nitrogen Data
+n_data <- nitrogen_data %>% 
+    mutate(percent_n_jan = percent_n_jan *100,
+           percent_n_may = percent_n_may *100,
+           percent_n_july = percent_n_july *100) %>% #turning them into %s 
+    pivot_longer(!1:5, names_to = "type", values_to = "percent_n") %>% 
+    separate(type, into = c("method","random", "date"), sep = "_") %>% 
+    dplyr::select(-random)
+
+#Pals
+# percent nitrogen 
+jan_data <- as.data.frame(rasterToPoints(spatial_brick[[1]]))
+pal_jan <- colorNumeric(palette = viridis((25), option = "plasma"), domain = jan_data$var1.pred, reverse = TRUE)
+
+may_data <- as.data.frame(rasterToPoints(spatial_brick[[2]]))
+pal_may <- colorNumeric(palette = viridis((25), option = "plasma"), domain = may_data$var1.pred, reverse = TRUE)
+
+july_data <- as.data.frame(rasterToPoints(spatial_brick[[3]]))
+pal_july <- colorNumeric(palette = viridis((25), option = "plasma"), domain = july_data$var1.pred, reverse = TRUE)
+
+# isotopic nitrogen 
+
+jan_i_data <- as.data.frame(rasterToPoints(spatial_brick[[4]]))
+pal_jan_i <- colorNumeric(palette = viridis((25), option = "plasma"), domain = jan_i_data$var1.pred, reverse = TRUE)
+
+may_i_data <- as.data.frame(rasterToPoints(spatial_brick[[5]]))
+pal_may_i <- colorNumeric(palette = viridis((25), option = "plasma"), domain = may_i_data$var1.pred, reverse = TRUE)
+
+july_i_data <- as.data.frame(rasterToPoints(spatial_brick[[6]]))
+pal_july_i <- colorNumeric(palette = viridis((25), option = "plasma"), domain = july_i_data$var1.pred, reverse = TRUE)
+
+bleach_data <- as.data.frame(rasterToPoints(spatial_brick[[7]]))
+pal_bleach <- colorNumeric(palette = viridis((25), option = "plasma"), domain = bleach_data$var1.pred, reverse = TRUE)
+
+# sewage 
+sew_dat <- as.data.frame(rasterToPoints(spatial_brick[[8]]))
+pal_sewage <- colorNumeric(palette = viridis((25), option = "plasma"), domain = sewage_data$var1.pred, reverse = TRUE)
+
+# lidar 
+# bathy_df <- as.data.frame(rasterToPoints(bathy_raster_filtered))
+# pal_bathy <- colorNumeric(palette = viridis((25), option = "plasma"), domain = bathy_df$layer, reverse = TRUE)
 
 # User Interface ----
 # Creates the structure for your app's look and appearance 
@@ -121,7 +167,11 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$leaflet_layers <- renderLeaflet({
-        leaflet()
+        leaflet(crs) %>% 
+            addProviderTiles("Esri.WorldImagery") %>%
+            addCircleMarkers(lng = n_data$longitude, lat = n_data$latitude,
+                                   color = "black", group = "Observations", radius = 1) %>% 
+            addRasterImage(spatial_brick[[1]], colors = "plasma", opacity = 0.7)
             
     })
 }
